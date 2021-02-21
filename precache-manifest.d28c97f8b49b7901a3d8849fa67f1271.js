a32c314abbe3f618b08562c09a5058ba0cef370b
Pavlo Buidenkov <pavlobu@gmail.com>
2021-02-21 12:33:18 +0200

    try

---
+++ precache-manifest.d28c97f8b49b7901a3d8849fa67f1271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97695d148ea4b21268af05fe6c164e42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9288a1549dc7e97c3ea7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b8a7d30c5dfa1fce79d2",
+    "url": "/static/js/2.a99da323.chunk.js"
+  },
+  {
+    "revision": "3cb25597d00277736ee1dc5452dc1b01",
+    "url": "/static/js/2.a99da323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9288a1549dc7e97c3ea7",
+    "url": "/static/js/main.bf5e04f3.chunk.js"
+  },
+  {
+    "revision": "d98d8112ca80e5704569",
+    "url": "/static/js/runtime-main.135d0715.js"
+  }
+]);
